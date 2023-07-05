605c4cd2016e45b404d93c5cfdf3f075484cba1e
Donald Gray <donald.gray@digirati.com>
2023-07-05 12:50:07 +0100

    Initial, basic implementation - dockerfile + logging

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build
+WORKDIR /src
+COPY ["src/IIIFAuth2/IIIFAuth2.API/IIIFAuth2.API.csproj", "IIIFAuth2.API/"]
+RUN dotnet restore "IIIFAuth2.API/IIIFAuth2.API.csproj"
+
+COPY src/IIIFAuth2/ .
+WORKDIR "/src/IIIFAuth2.API"
+RUN dotnet build "IIIFAuth2.API.csproj" -c Release -o /app/build
+
+FROM build AS publish
+RUN dotnet publish "IIIFAuth2.API.csproj" -c Release -o /app/publish
+
+FROM mcr.microsoft.com/dotnet/aspnet:6.0-bullseye-slim AS base
+
+LABEL maintainer="Donald Gray <donald.gray@digirati.com>"
+LABEL org.opencontainers.image.source=https://github.com/dlcs/iiif-auth-v2
+LABEL org.opencontainers.image.description="IIIF Auth API v2 implementation for DLCS."
+
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "IIIFAuth2.API.dll"]
--- src/IIIFAuth2/IIIFAuth2.API/Program.cs
+++ src/IIIFAuth2/IIIFAuth2.API/Program.cs
@@ -1,6 +1,37 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Serilog;
 
-app.MapGet("/", () => "Hello World!");
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-app.Run();
+Log.Information("Application starting..");
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Configure serilog
+    builder.Host.UseSerilog((hostContext, loggerConfiguration)
+        => loggerConfiguration
+            .ReadFrom.Configuration(hostContext.Configuration)
+            .Enrich.FromLogContext()
+            .Enrich.WithCorrelationIdHeader());
+
+    builder.Services.AddHttpContextAccessor();
+
+    var app = builder.Build();
+    app.UseSerilogRequestLogging();
+
+    app.MapGet("/", () => "Hello World!");
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Unhandled exception on startup");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
+}
