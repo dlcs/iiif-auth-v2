e6a4d70a43edfe962a68fd7f2cdf20e92046c5c9
Donald Gray <donald.gray@digirati.com>
2023-07-18 12:00:03 +0100

    Add assetId + collectionX helpers

---
+++ src/IIIFAuth2/IIIFAuth2.API.Tests/Utils/CollectionXTests.cs
@@ -0,0 +1,30 @@
+﻿using IIIFAuth2.API.Utils;
+
+namespace IIIFAuth2.API.Tests.Utils;
+
+public class CollectionXTests
+{
+    [Fact]
+    public void IsNullOrEmpty_List_True_IfNull()
+    {
+        List<int>? coll = null;
+
+        coll.IsNullOrEmpty().Should().BeTrue();
+    }
+    
+    [Fact]
+    public void IsNullOrEmpty_List_True_IfEmpty()
+    {
+        var coll = new List<int>();
+
+        coll.IsNullOrEmpty().Should().BeTrue();
+    }
+    
+    [Fact]
+    public void IsNullOrEmpty_List_False_IfHasValues()
+    {
+        var coll = new List<int> {2};
+
+        coll.IsNullOrEmpty().Should().BeFalse();
+    }
+}
---
+++ src/IIIFAuth2/IIIFAuth2.API/Models/AssetId.cs
@@ -0,0 +1,25 @@
+﻿namespace IIIFAuth2.API.Models;
+
+/// <summary>
+/// A record that represents an identifier for a DLCS Asset.
+/// </summary>
+public record AssetId(int Customer, int Space, string Asset)
+{
+    public override string ToString() => $"{Customer}/{Space}/{Asset}";
+    
+    public static AssetId FromString(string assetImageId)
+    {
+        var parts = assetImageId.Split("/", StringSplitOptions.RemoveEmptyEntries);
+        
+        try
+        {
+            return new AssetId(int.Parse(parts[0]), int.Parse(parts[1]), parts[2]);
+        }
+        catch (FormatException fmEx)
+        {
+            throw new FormatException(
+                $"AssetId '{assetImageId}' is invalid. Must be in format customer/space/asset",
+                fmEx);
+        }
+    }
+}
