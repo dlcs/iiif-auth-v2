550fbddd0fbd5e0537f5c5f647e36c1a3c45ebc4
Donald Gray <donald.gray@digirati.com>
2023-07-18 12:11:17 +0100

    Add basic accessService converter

---
+++ src/IIIFAuth2/IIIFAuth2.API.Tests/Models/Converters/AccessServiceConverterTests.cs
@@ -0,0 +1,97 @@
+﻿using IIIF;
+using IIIF.Auth.V2;
+using IIIF.Presentation.V3.Strings;
+using IIIFAuth2.API.Data.Entities;
+using IIIFAuth2.API.Models.Converters;
+
+namespace IIIFAuth2.API.Tests.Models.Converters;
+
+public class AccessServiceConverterTests
+{
+    [Fact]
+    public void ToIIIFModel_ConvertsMinimalAccessService()
+    {
+        // Arrange
+        var accessService = new AccessService
+        {
+            Id = Guid.Empty,
+            Profile = "active",
+            Customer = 99,
+            Name = "test access service",
+        };
+
+        var expected = new AuthAccessService2
+        {
+            Id = "todo",
+            Profile = "active",
+            Service = new List<IService>
+            {
+                new AuthAccessTokenService2
+                {
+                    Id = "todo",
+                },
+                new AuthLogoutService2
+                {
+                    Id = "todo",
+                    Label = new LanguageMap("en", "Logout of test access service")
+                }
+            }
+        };
+
+        // Act
+        var actual = accessService.ToIIIFModel();
+        
+        // Assert
+        actual.Should().BeEquivalentTo(expected);
+    }
+    
+    [Fact]
+    public void ToIIIFModel_ConvertsFullAccessService()
+    {
+        // Arrange
+        var accessService = new AccessService
+        {
+            Id = Guid.Empty,
+            Profile = "active",
+            Customer = 99,
+            Name = "test access service",
+            ConfirmLabel = new LanguageMap("en", "confirm"),
+            Label = new LanguageMap("en", "label"),
+            Heading = new LanguageMap("en", "heading"),
+            Note = new LanguageMap("en", "note"),
+            LogoutLabel = new LanguageMap("en", "logout-label"),
+            AccessTokenErrorHeading = new LanguageMap("en", "error heading"),
+            AccessTokenErrorNote = new LanguageMap("en", "error note"),
+        };
+
+        var expected = new AuthAccessService2
+        {
+            Id = "todo",
+            Profile = "active",
+            Label = new LanguageMap("en", "label"),
+            Heading = new LanguageMap("en", "heading"),
+            Note = new LanguageMap("en", "note"),
+            ConfirmLabel = new LanguageMap("en", "confirm"),
+            Service = new List<IService>
+            {
+                new AuthAccessTokenService2
+                {
+                    Id = "todo",
+                    ErrorHeading = new LanguageMap("en", "error heading"),
+                    ErrorNote = new LanguageMap("en", "error note"),
+                },
+                new AuthLogoutService2
+                {
+                    Id = "todo",
+                    Label = new LanguageMap("en", "logout-label"),
+                }
+            }
+        };
+
+        // Act
+        var actual = accessService.ToIIIFModel();
+        
+        // Assert
+        actual.Should().BeEquivalentTo(expected);
+    }
+}
---
+++ src/IIIFAuth2/IIIFAuth2.API/Models/Converters/AccessServiceConverter.cs
@@ -0,0 +1,52 @@
+﻿using IIIF;
+using IIIF.Auth.V2;
+using IIIF.Presentation.V3.Strings;
+using IIIFAuth2.API.Data.Entities;
+
+namespace IIIFAuth2.API.Models.Converters;
+
+public static class AccessServiceConverter
+{
+    /// <summary>
+    /// Converts database entity <see cref="AccessService"/> to <see cref="AuthAccessService2"/>
+    /// </summary>
+    public static AuthAccessService2 ToIIIFModel(this AccessService accessService)
+    {
+        var authAccessService = new AuthAccessService2
+        {
+            Id = "todo", // TODO
+            Profile = accessService.Profile,
+            Label = accessService.Label,
+            Heading = accessService.Heading,
+            Note = accessService.Note,
+            ConfirmLabel = accessService.ConfirmLabel,
+            Service = new List<IService>
+            {
+                GenerateTokenService(accessService),
+                GenerateLogoutService(accessService),
+            },
+        };
+        return authAccessService;
+    }
+
+    private static IService GenerateTokenService(AccessService accessService)
+    {
+        var authTokenService = new AuthAccessTokenService2
+        {
+            Id = "todo", // TODO
+            ErrorHeading = accessService.AccessTokenErrorHeading,
+            ErrorNote = accessService.AccessTokenErrorNote,
+        };
+        return authTokenService;
+    }
+    
+    private static IService GenerateLogoutService(AccessService accessService)
+    {
+        var logoutService = new AuthLogoutService2
+        {
+            Id = "todo", // TODO
+            Label = accessService.LogoutLabel ?? new LanguageMap("en", $"Logout of {accessService.Name}")
+        };
+        return logoutService;
+    }
+}
