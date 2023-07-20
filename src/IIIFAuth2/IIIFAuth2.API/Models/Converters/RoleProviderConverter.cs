4bddfb35652570a507b16a53946e24b62226ce2f
Donald Gray <donald.gray@digirati.com>
2023-07-20 16:45:24 +0100

    Add strongly typed RoleProviderConfig object

---
+++ src/IIIFAuth2/IIIFAuth2.API/Models/Converters/RoleProviderConverter.cs
@@ -0,0 +1,48 @@
+﻿using IIIFAuth2.API.Models.Domain;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace IIIFAuth2.API.Models.Converters;
+
+/// <summary>
+/// Polymorphic <see cref="JsonConverter{T}"/> for <see cref="IProviderConfiguration"/> objects 
+/// </summary>
+public class RoleProviderConverter : JsonConverter<IProviderConfiguration>
+{
+    public override void WriteJson(JsonWriter writer, IProviderConfiguration? value, JsonSerializer serializer)
+    {
+        var thisIndex = serializer.Converters.IndexOf(this);
+        serializer.Converters.RemoveAt(thisIndex);
+        serializer.Serialize(writer, value);
+        serializer.Converters.Insert(thisIndex, this);
+    }
+
+    public override IProviderConfiguration ReadJson(JsonReader reader, Type objectType, IProviderConfiguration? existingValue,
+        bool hasExistingValue, JsonSerializer serializer)
+    {
+        var jsonObject = JObject.Load(reader);
+
+        var providerConfiguration = CreateConcreteObject(jsonObject);
+
+        serializer.Populate(jsonObject.CreateReader(), providerConfiguration);
+        return providerConfiguration;
+    }
+
+    private static IProviderConfiguration CreateConcreteObject(JObject jsonObject)
+    {
+        var configJToken = jsonObject["config"];
+        var configValue = configJToken?.ToObject<RoleProviderType>() ?? RoleProviderType.Unknown;
+
+        switch (configValue)
+        {
+            case RoleProviderType.Clickthrough:
+                return new ClickthroughConfiguration();
+            case RoleProviderType.Oidc:
+                return new OidcConfiguration();
+            case RoleProviderType.Unknown:
+            default:
+                throw new ArgumentOutOfRangeException(
+                    $"Could not determine RoleProvider config type for {configJToken ?? "config-key-not-found"}");
+        }
+    }
+}
---
+++ src/IIIFAuth2/IIIFAuth2.API/Models/Domain/RoleProviderConfiguration.cs
@@ -0,0 +1,146 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace IIIFAuth2.API.Models.Domain;
+
+/// <summary>
+/// A collection of <see cref="IProviderConfiguration"/> objects, keyed as dictionary.
+/// </summary>
+/// <remarks>
+/// The key will always be "default" but is a placeholder to allow more flexible configuration in the future. For auth1
+/// it stored hostname as different RoleProviders could be used per host
+/// </remarks>
+public class RoleProviderConfiguration : Dictionary<string, IProviderConfiguration>
+{
+}
+
+/// <summary>
+/// Marker interface role-provider properties that are common to all types
+/// </summary>
+public interface IProviderConfiguration
+{
+    /// <summary>
+    /// Optional title to be displayed on popup to capture significant gesture
+    /// </summary>
+    public string? GestureTitle { get; set; }
+    
+    /// <summary>
+    /// Optional title to be displayed on popup to capture significant gesture
+    /// </summary>
+    public string? GestureMessage { get; set; }
+
+    /// <summary>
+    /// The type of configuration, this determines how it is handled
+    /// </summary>
+    public RoleProviderType Config { get; set; }
+}
+
+/// <summary>
+/// Configuration for clickthrough authentication, which the DLCS handles itself without any external system 
+/// </summary>
+public class ClickthroughConfiguration : IProviderConfiguration
+{
+    /// <summary>
+    /// <inheritdoc />
+    /// </summary>
+    public string? GestureTitle { get; set; }
+    
+    /// <summary>
+    /// <inheritdoc />
+    /// </summary>
+    public string? GestureMessage { get; set; }
+    
+    /// <summary>
+    /// <inheritdoc />
+    /// </summary>
+    [JsonConverter(typeof(StringEnumConverter))]
+    public RoleProviderType Config { get; set; }
+}
+
+/// <summary>
+/// Configuration for using oidc to authenticate user and appropriate roles
+/// </summary>
+/// <remarks>
+/// See https://github.com/dlcs/protagonist/blob/main/docs/rfcs/008-more-access-control-oidc-oauth.md#role-provider---oidc
+/// </remarks>
+public class OidcConfiguration : IProviderConfiguration
+{
+    public string Provider { get; set; } = null!;
+    public string Domain { get; set; } = null!;
+    public string Scopes { get; set; } = null!;
+    public string ClaimType { get; set; } = null!;
+    
+    /// <summary>
+    /// How to handle an unknown claim value 
+    /// </summary>
+    [JsonConverter(typeof(StringEnumConverter))]
+    public UnknownMappingValueBehaviour UnknownValueBehaviour { get; set; }
+
+    /// <summary>
+    /// Role(s) to use if <see cref="UnknownMappingValueBehaviour"/> is 'Fallback'
+    /// </summary>
+    public string[]? FallbackMapping { get; set; }
+    
+    /// <summary>
+    /// A collection of {claimValue}:{dlcs-role} mappings
+    /// </summary>
+    public Dictionary<string, string[]> Mapping { get; set; } = new();
+    
+    /// <summary>
+    /// <inheritdoc />
+    /// </summary>
+    public string? GestureTitle { get; set; }
+    
+    /// <summary>
+    /// <inheritdoc />
+    /// </summary>
+    public string? GestureMessage { get; set; }
+    
+    /// <summary>
+    /// <inheritdoc />
+    /// </summary>
+    [JsonConverter(typeof(StringEnumConverter))]
+    public RoleProviderType Config { get; set; }
+}
+
+/// <summary>
+/// How to handle an unknown claim value for oidc role-provider
+/// </summary>
+public enum UnknownMappingValueBehaviour
+{
+    /// <summary>
+    /// Default placeholder
+    /// </summary>
+    Unknown,
+    
+    /// <summary>
+    /// Throw an exception, prevent user logging in
+    /// </summary>
+    Throw,
+    
+    /// <summary>
+    /// Use the claim value as-is
+    /// </summary>
+    UseClaim,
+    
+    /// <summary>
+    /// Use a default, fallback value as defined in <see cref="OidcConfiguration.FallbackMapping"/>
+    /// </summary>
+    Fallback,
+}
+
+/// <summary>
+/// Details the type of role-provider, this will dictate how it is handled and what type it's mapped to
+/// </summary>
+public enum RoleProviderType
+{
+    Unknown,
+    Clickthrough,
+    Oidc
+}
+
+/*
+"default": {
+  "config": "clickthrough"
+}
+*/
