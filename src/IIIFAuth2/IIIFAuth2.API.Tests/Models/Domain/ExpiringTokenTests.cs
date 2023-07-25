509a89e48a53e847468c6ab050b580511dd0ad12
Donald Gray <donald.gray@digirati.com>
2023-07-25 14:04:02 +0100

    Add ExpiringToken

---
+++ src/IIIFAuth2/IIIFAuth2.API.Tests/Models/Domain/ExpiringTokenTests.cs
@@ -0,0 +1,40 @@
+﻿using IIIFAuth2.API.Models.Domain;
+
+namespace IIIFAuth2.API.Tests.Models.Domain;
+
+public class ExpiringTokenTests
+{
+    [Fact]
+    public void GenerateNewNonce_Throws_IfNonUtcDatePassed()
+    {
+        Action action = () => ExpiringToken.GenerateNewToken(DateTime.Now);
+        action.Should().Throw<ArgumentException>()
+            .WithParameterName("timestamp");
+    }
+    
+    [Fact]
+    public void GenerateNewNonce_CreatesDifferentTokens_ForSametime()
+    {
+        var datetime = DateTime.UtcNow;
+        var token1 = ExpiringToken.GenerateNewToken(datetime);
+        var token2 = ExpiringToken.GenerateNewToken(datetime);
+        
+        token1.Should().NotBe(token2);
+    }
+
+    [Fact]
+    public void HasExpired_False_ForNewToken()
+    {
+        var token = ExpiringToken.GenerateNewToken();
+
+        ExpiringToken.HasExpired(token).Should().BeFalse();
+    }
+    
+    [Fact]
+    public void HasExpired_True_IfExpired()
+    {
+        var token = ExpiringToken.GenerateNewToken(DateTime.UtcNow.AddHours(-100));
+
+        ExpiringToken.HasExpired(token, 90).Should().BeTrue();
+    }
+}
