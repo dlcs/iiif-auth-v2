3d17fb98593f2c69243edcf27cd2b42e1fd04502
Donald Gray <donald.gray@digirati.com>
2023-07-28 17:17:24 +0100

    SessionUser gains origin

--- src/IIIFAuth2/IIIFAuth2.API/Data/Entities/SessionUser.cs
+++ src/IIIFAuth2/IIIFAuth2.API/Data/Entities/SessionUser.cs
@@ -8,10 +8,33 @@ public class SessionUser : IHaveCustomer
 {
     public Guid Id { get; set; }
     public int Customer { get; set; }
+    
+    /// <summary>
+    /// This value is set in the cookie (authorizing aspect)
+    /// </summary>
     public string CookieId { get; set; } = null!;
+    
+    /// <summary>
+    /// The origin that was sent when creating cookie
+    /// </summary>
+    public string Origin { get; set; } = null!;
+    
+    /// <summary>
+    /// Token returned from AccessTokenService, used in requests to ProbeService
+    /// </summary>
     public string AccessToken { get; set; } = null!;
+    
     public DateTime Expires { get; set; }
+    
     public DateTime Created { get; set; }
+    
+    /// <summary>
+    /// When expiry last checked
+    /// </summary>
     public DateTime? LastChecked { get; set; }
+    
+    /// <summary>
+    /// List of roles this session has access to
+    /// </summary>
     public List<string> Roles { get; set; } = new();
 }
