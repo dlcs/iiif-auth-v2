fd30a0405c6a9dc18277499226070353eb47e012
Donald Gray <donald.gray@digirati.com>
2023-07-31 13:59:13 +0100

    Add access token service

---
+++ src/IIIFAuth2/IIIFAuth2.API/Features/Access/AuthServiceBuilder.cs
@@ -0,0 +1,15 @@
+﻿using IIIF.Auth.V2;
+using IIIF.Presentation.V3.Strings;
+
+namespace IIIFAuth2.API.Features.Access;
+
+internal static class AuthServiceBuilder
+{
+    public static AuthAccessTokenError2 CreateAuthAccessTokenError2(string profile, string messageId, string heading,
+        string note)
+        => new(profile, new LanguageMap("en", note))
+        {
+            Heading = new LanguageMap("en", heading),
+            MessageId = messageId,
+        };
+}
