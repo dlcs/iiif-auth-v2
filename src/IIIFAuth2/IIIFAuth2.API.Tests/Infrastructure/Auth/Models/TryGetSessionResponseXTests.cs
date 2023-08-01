519de3f3ed6a47c8ba3efc456120b3a577885e0e
Donald Gray <donald.gray@digirati.com>
2023-08-01 14:31:27 +0100

    Method for testing if userSession has requested role(s)

--- src/IIIFAuth2/IIIFAuth2.API.Tests/Infrastructure/Auth/Models/TryGetSessionResponseXTests.cs
+++ src/IIIFAuth2/IIIFAuth2.API.Tests/Infrastructure/Auth/Models/TryGetSessionResponseXTests.cs
@@ -40,4 +40,80 @@ public class TryGetSessionResponseXTests
         // Assert
         tryGetSession.IsSuccessWithSession().Should().BeTrue();
     }
+
+    [Theory]
+    [InlineData(GetSessionStatus.MissingSession)]
+    [InlineData(GetSessionStatus.DifferentOrigin)]
+    [InlineData(GetSessionStatus.MissingCredentials)]
+    [InlineData(GetSessionStatus.InvalidCookie)]
+    [InlineData(GetSessionStatus.ExpiredSession)]
+    [InlineData(GetSessionStatus.UnknownError)]
+    public void CanUserAccessAtLeastOneRole_False_IfNotSuccess(GetSessionStatus status)
+    {
+        // Arrange
+        var roles = new List<string> { "foo" };
+        var tryGetSession = new TryGetSessionResponse(status, new SessionUser { Roles = roles });
+        
+        // Assert
+        tryGetSession.CanUserAccessAtLeastOneRole(roles).Should().BeFalse();
+    }
+    
+    [Fact]
+    public void CanUserAccessAtLeastOneRole_False_IfNoSessionUser()
+    {
+        // Arrange
+        var roles = new List<string> { "foo" };
+        var tryGetSession = new TryGetSessionResponse(GetSessionStatus.Success);
+        
+        // Assert
+        tryGetSession.CanUserAccessAtLeastOneRole(roles).Should().BeFalse();
+    }
+    
+    [Fact]
+    public void CanUserAccessAtLeastOneRole_False_NullRoles()
+    {
+        // Arrange
+        var roles = new List<string> { "foo" };
+        var tryGetSession = new TryGetSessionResponse(GetSessionStatus.Success, new SessionUser());
+        
+        // Assert
+        tryGetSession.CanUserAccessAtLeastOneRole(roles).Should().BeFalse();
+    }
+    
+    [Fact]
+    public void CanUserAccessAtLeastOneRole_False_EmptyRoles()
+    {
+        // Arrange
+        var roles = new List<string> { "foo" };
+        var tryGetSession =
+            new TryGetSessionResponse(GetSessionStatus.Success, new SessionUser { Roles = new List<string>(0) });
+        
+        // Assert
+        tryGetSession.CanUserAccessAtLeastOneRole(roles).Should().BeFalse();
+    }
+    
+    [Fact]
+    public void CanUserAccessAtLeastOneRole_False_NoMatches()
+    {
+        // Arrange
+        var roles = new List<string> { "foo" };
+        var tryGetSession =
+            new TryGetSessionResponse(GetSessionStatus.Success, new SessionUser { Roles = new List<string> { "bar" } });
+        
+        // Assert
+        tryGetSession.CanUserAccessAtLeastOneRole(roles).Should().BeFalse();
+    }
+    
+    [Fact]
+    public void CanUserAccessAtLeastOneRole_True_IfMatch()
+    {
+        // Arrange
+        var roles = new List<string> { "foo", "bar" };
+        var tryGetSession =
+            new TryGetSessionResponse(GetSessionStatus.Success,
+                new SessionUser { Roles = new List<string> { "baz", "bar" } });
+        
+        // Assert
+        tryGetSession.CanUserAccessAtLeastOneRole(roles).Should().BeTrue();
+    }
 }
--- src/IIIFAuth2/IIIFAuth2.API/Infrastructure/Auth/Models/TryGetSessionResponse.cs
+++ src/IIIFAuth2/IIIFAuth2.API/Infrastructure/Auth/Models/TryGetSessionResponse.cs
@@ -1,4 +1,5 @@
 ﻿using IIIFAuth2.API.Data.Entities;
+using IIIFAuth2.API.Utils;
 
 namespace IIIFAuth2.API.Infrastructure.Auth.Models;
 
@@ -14,6 +15,18 @@ public static class TryGetSessionResponseX
     /// </summary>
     public static bool IsSuccessWithSession(this TryGetSessionResponse response)
         => response.Status == GetSessionStatus.Success && response.SessionUser != null;
+
+    /// <summary>
+    /// Check if current Session has access to at least one of the specified roles
+    /// </summary>
+    public static bool CanUserAccessAtLeastOneRole(this TryGetSessionResponse response,
+        IEnumerable<string> roles)
+    {
+        if (!response.IsSuccessWithSession()) return false;
+        
+        var sessionUserRoles = response.SessionUser?.Roles;
+        return !sessionUserRoles.IsNullOrEmpty() && sessionUserRoles.Intersect(roles).Any();
+    }
 }
 
 public enum GetSessionStatus
